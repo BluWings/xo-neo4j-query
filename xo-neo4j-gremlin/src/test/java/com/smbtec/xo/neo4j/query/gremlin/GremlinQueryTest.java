992393c038d339445287fe963c7f072ed414d57d
Lars Martin <lars.martin@smb-tec.com>
2014-05-19 10:28:04 +0200

    remove legacy index setup from test
    Signed-off-by: Lars Martin <lars.martin@smb-tec.com>

--- xo-neo4j-gremlin/src/test/java/com/smbtec/xo/neo4j/query/gremlin/GremlinQueryTest.java
+++ xo-neo4j-gremlin/src/test/java/com/smbtec/xo/neo4j/query/gremlin/GremlinQueryTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -109,13 +109,6 @@ public class GremlinQueryTest extends AbstractXOManagerTest {
     public void setup() {
         final XOManager xoManager = getXoManager();
         xoManager.currentTransaction().begin();
-
-        // enable legacy auto indexing
-        final Neo4jDatastoreSession datastoreSession = xoManager.getDatastoreSession(Neo4jDatastoreSession.class);
-        final GraphDatabaseService graphDatabaseService = datastoreSession.getGraphDatabaseService();
-        final AutoIndexer<Node> nodeAutoIndexer = graphDatabaseService.index().getNodeAutoIndexer();
-        nodeAutoIndexer.startAutoIndexingProperty("name");
-        nodeAutoIndexer.setEnabled(true);
 
         final A a = xoManager.create(A.class);
         a.setName("foo");
